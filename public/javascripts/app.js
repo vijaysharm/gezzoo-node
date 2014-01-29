af6933849aecce7a97f0c4a7d36970e3bdb8b396
Vijay Sharma <vijay.sharm@gmail.com>
2014-01-28 22:32:15 -0500

    About to move the client application over to Ember

---
+++ public/javascripts/app.js
@@ -0,0 +1,330 @@
+Data = {};
+Data.User = {
+	"name":"gezzoo_1",
+	"token":"52728fbf63a64c904c657ed5",
+	"id":"52728fbf63a64c904c657ed5"
+};
+
+Data.Games = [{
+	"_id":"5286e01d9beb41000000001c",
+	"me":{
+		"username":"gezzoo_1",
+		"_id":"52728fbf63a64c904c657ed5",
+		"board":[
+			{"_id":"5386e01d8b587b0000000001","up":true},
+			{"_id":"5486e01d8b587b0000000001","up":true},
+			{"_id":"5586e01d8b587b0000000001","up":true},
+			{"_id":"5686e01d8b587b0000000001","up":true},
+			{"_id":"5786e01d8b587b0000000001","up":true},
+			{"_id":"5886e01d8b587b0000000001","up":true},
+			{"_id":"5986e01d8b587b0000000001","up":true},
+			{"_id":"5a86e01d8b587b0000000001","up":true},
+			{"_id":"5b86e01d8b587b0000000001","up":true},
+			{"_id":"5c86e01d8b587b0000000001","up":true},
+			{"_id":"5d86e01d8b587b0000000001","up":true},
+			{"_id":"5e86e01d8b587b0000000001","up":true},
+			{"_id":"5f86e01d8b587b0000000001","up":true},
+			{"_id":"5316e01d8b587b0000000001","up":true},
+			{"_id":"5326e01d8b587b0000000001","up":true},
+			{"_id":"5336e01d8b587b0000000001","up":true},
+			{"_id":"5346e01d8b587b0000000001","up":true},
+			{"_id":"5356e01d8b587b0000000001","up":true},
+			{"_id":"5366e01d8b587b0000000001","up":true},
+			{"_id":"5376e01d8b587b0000000001","up":true},
+			{"_id":"5386e01d8b587b0000000002","up":true},
+			{"_id":"5396e01d8b587b0000000002","up":true},
+			{"_id":"53a6e01d8b587b0000000001","up":true},
+			{"_id":"53b6e01d8b587b0000000001","up":true}
+		],
+		"character":"5486e01d8b587b0000000001"
+	},
+	"opponent":{
+		"username":"gezzoo_0",
+		"_id":"52728ca9954deb0b31000004"
+	},
+	"board":{
+		"_id":"5286fd942200ab0000000002",
+		"name":"Bollywood",
+		"characters":[
+			"5386e01d8b587b0000000001",
+			"5486e01d8b587b0000000001",
+			"5586e01d8b587b0000000001",
+			"5686e01d8b587b0000000001",
+			"5786e01d8b587b0000000001",
+			"5886e01d8b587b0000000001",
+			"5986e01d8b587b0000000001",
+			"5a86e01d8b587b0000000001",
+			"5b86e01d8b587b0000000001",
+			"5c86e01d8b587b0000000001",
+			"5d86e01d8b587b0000000001",
+			"5e86e01d8b587b0000000001",
+			"5f86e01d8b587b0000000001",
+			"5316e01d8b587b0000000001",
+			"5326e01d8b587b0000000001",
+			"5336e01d8b587b0000000001",
+			"5346e01d8b587b0000000001",
+			"5356e01d8b587b0000000001",
+			"5366e01d8b587b0000000001",
+			"5376e01d8b587b0000000001",
+			"5386e01d8b587b0000000002",
+			"5396e01d8b587b0000000002",
+			"53a6e01d8b587b0000000001",
+			"53b6e01d8b587b0000000001"
+		]
+	},
+	"turn":"52728ca9954deb0b31000004",
+	"ended":false,
+	"modified":"2014-01-15T00:42:42.997Z"
+},
+{
+	"_id":"5286e01d9beb41000b00003a",
+	"me":{
+		"username":"gezzoo_1",
+		"_id":"52728fbf63a64c904c657ed5",
+		"board":[
+			{"_id":"5386e01d8b587b0000000001","up":true},
+			{"_id":"5486e01d8b587b0000000001","up":true},
+			{"_id":"5586e01d8b587b0000000001","up":true},
+			{"_id":"5686e01d8b587b0000000001","up":true},
+			{"_id":"5786e01d8b587b0000000001","up":true},
+			{"_id":"5886e01d8b587b0000000001","up":true},
+			{"_id":"5986e01d8b587b0000000001","up":true},
+			{"_id":"5a86e01d8b587b0000000001","up":true},
+			{"_id":"5b86e01d8b587b0000000001","up":true},
+			{"_id":"5c86e01d8b587b0000000001","up":true},
+			{"_id":"5d86e01d8b587b0000000001","up":true},
+			{"_id":"5e86e01d8b587b0000000001","up":true},
+			{"_id":"5f86e01d8b587b0000000001","up":true},
+			{"_id":"5316e01d8b587b0000000001","up":true},
+			{"_id":"5326e01d8b587b0000000001","up":true},
+			{"_id":"5336e01d8b587b0000000001","up":true},
+			{"_id":"5346e01d8b587b0000000001","up":true},
+			{"_id":"5356e01d8b587b0000000001","up":true},
+			{"_id":"5366e01d8b587b0000000001","up":true},
+			{"_id":"5376e01d8b587b0000000001","up":true},
+			{"_id":"5386e01d8b587b0000000002","up":true},
+			{"_id":"5396e01d8b587b0000000002","up":true},
+			{"_id":"53a6e01d8b587b0000000001","up":true},
+			{"_id":"53b6e01d8b587b0000000001","up":true}
+		],
+		"character":"5486e01d8b587b0000000001"
+	},
+	"opponent":{
+		"username":"gezzoo_2",
+		"_id":"52728fbf63a64c904c657ea6"
+	},
+	"board":{
+		"_id":"5286fd942200ab0000000002",
+		"name":"Bollywood",
+		"characters":[
+			"5386e01d8b587b0000000001",
+			"5486e01d8b587b0000000001",
+			"5586e01d8b587b0000000001",
+			"5686e01d8b587b0000000001",
+			"5786e01d8b587b0000000001",
+			"5886e01d8b587b0000000001",
+			"5986e01d8b587b0000000001",
+			"5a86e01d8b587b0000000001",
+			"5b86e01d8b587b0000000001",
+			"5c86e01d8b587b0000000001",
+			"5d86e01d8b587b0000000001",
+			"5e86e01d8b587b0000000001",
+			"5f86e01d8b587b0000000001",
+			"5316e01d8b587b0000000001",
+			"5326e01d8b587b0000000001",
+			"5336e01d8b587b0000000001",
+			"5346e01d8b587b0000000001",
+			"5356e01d8b587b0000000001",
+			"5366e01d8b587b0000000001",
+			"5376e01d8b587b0000000001",
+			"5386e01d8b587b0000000002",
+			"5396e01d8b587b0000000002",
+			"53a6e01d8b587b0000000001",
+			"53b6e01d8b587b0000000001"
+		]
+	},
+	"turn":"52728fbf63a64c904c657ed5",
+	"ended":false,
+	"modified":"2014-01-15T00:42:42.998Z"
+}];
+
+// $.getJSON('/api/games/5286e01d9beb41000b00003a?token=52728fbf63a64c904c657ed5', function(result){console.log(result)});
+Data.Game = {
+	"_id":"5286e01d9beb41000b00003a",
+	"me": {
+		"username":"gezzoo_1",
+		"_id":"52728fbf63a64c904c657ed5",
+		"board":[
+			{"_id":"5386e01d8b587b0000000001","up":true},
+			{"_id":"5486e01d8b587b0000000001","up":true},
+			{"_id":"5586e01d8b587b0000000001","up":true},
+			{"_id":"5686e01d8b587b0000000001","up":true},
+			{"_id":"5786e01d8b587b0000000001","up":true},
+			{"_id":"5886e01d8b587b0000000001","up":true},
+			{"_id":"5986e01d8b587b0000000001","up":true},
+			{"_id":"5a86e01d8b587b0000000001","up":true},
+			{"_id":"5b86e01d8b587b0000000001","up":true},
+			{"_id":"5c86e01d8b587b0000000001","up":true},
+			{"_id":"5d86e01d8b587b0000000001","up":true},
+			{"_id":"5e86e01d8b587b0000000001","up":true},
+			{"_id":"5f86e01d8b587b0000000001","up":true},
+			{"_id":"5316e01d8b587b0000000001","up":true},
+			{"_id":"5326e01d8b587b0000000001","up":true},
+			{"_id":"5336e01d8b587b0000000001","up":true},
+			{"_id":"5346e01d8b587b0000000001","up":true},
+			{"_id":"5356e01d8b587b0000000001","up":true},
+			{"_id":"5366e01d8b587b0000000001","up":true},
+			{"_id":"5376e01d8b587b0000000001","up":true},
+			{"_id":"5386e01d8b587b0000000002","up":true},
+			{"_id":"5396e01d8b587b0000000002","up":true},
+			{"_id":"53a6e01d8b587b0000000001","up":true},
+			{"_id":"53b6e01d8b587b0000000001","up":true}
+		],
+		"character":"5486e01d8b587b0000000001",
+		"actions":[
+			{
+				"_id":"52d5d982a4be878f02000003",
+				"gameid":"5286e01d9beb41000b00003a",
+				"by":"52728fbf63a64c904c657ed5",
+				"action":"question",
+				"value":"Are you there?",
+				"reply":{"value":"yes"},
+				"modified":"2014-01-15T00:42:42.998Z"
+			}
+		]
+	},
+	"opponent":{
+		"username":"gezzoo_2",
+		"_id":"52728fbf63a64c904c657ea6",
+		"actions":[
+			{
+				"_id":"52d5d982a4be878f02000001",
+				"gameid":"5286e01d9beb41000b00003a",
+				"by":"52728fbf63a64c904c657ea6",
+				"action":"question",
+				"value":"Are you there?",
+				"modified":"2014-01-15T00:42:42.998Z"
+			},
+			{
+				"_id":"52d5d982a4be878f02000002",
+				"gameid":"5286e01d9beb41000b00003a",
+				"by":"52728fbf63a64c904c657ea6",
+				"action":"question",
+				"value":"Are you there?",
+				"reply":{"value":"yes"},
+				"modified":"2014-01-15T00:42:42.998Z"
+			}]
+	},
+	"board":{
+		"name":"Bollywood",
+		"characters":[
+			{"_id":"5316e01d8b587b0000000001","name":"Katrina Kaif","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTUwODY3NzA3NF5BMl5BanBnXkFtZTcwNTEzNDg3OA@@._V1_SY317_CR6,0,214,317_.jpg"},
+			{"_id":"5326e01d8b587b0000000001","name":"Anushka Sharma","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTc0NDMzMDYyM15BMl5BanBnXkFtZTcwMDEwMTA1NA@@._V1_SY317_CR104,0,214,317_.jpg"},
+			{"_id":"5336e01d8b587b0000000001","name":"Priyanka Chopra","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMjAxNzUwNjExOV5BMl5BanBnXkFtZTcwNDUyMTUxNw@@._V1_SY317_CR105,0,214,317_.jpg"},
+			{"_id":"5346e01d8b587b0000000001","name":"Aishwarya Rai","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMjEyMjEyODkzN15BMl5BanBnXkFtZTcwODkxMTY1Mg@@._V1_SX214_CR0,0,214,317_.jpg"},
+			{"_id":"5356e01d8b587b0000000001","name":"Sonakshi Sinha","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BOTg3MzQxMTkwOF5BMl5BanBnXkFtZTcwNjcyOTM5NA@@._V1_SY317_CR18,0,214,317_.jpg"},
+			{"_id":"5366e01d8b587b0000000001","name":"Vidya Balan","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BNDI3Mjk2MjgzMl5BMl5BanBnXkFtZTcwODQwMjI1OQ@@._V1_SY317_CR3,0,214,317_.jpg"},
+			{"_id":"5376e01d8b587b0000000001","name":"Rani Mukerji","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTg0MTI5MDkyMV5BMl5BanBnXkFtZTcwNzIyMDQ4Mg@@._V1_SY317_CR111,0,214,317_.jpg"},
+			{"_id":"5386e01d8b587b0000000001","name":"Shah Rukh Khan","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTQxMjg4Mzk1Nl5BMl5BanBnXkFtZTcwMzQyMTUxNw@@._V1_SY317_CR1,0,214,317_.jpg"},
+			{"_id":"5386e01d8b587b0000000002","name":"Preity Zinta","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTQyMDc4NTE1Ml5BMl5BanBnXkFtZTcwOTQwMDgxOA@@._V1_SX214_CR0,0,214,317_.jpg"},
+			{"_id":"5396e01d8b587b0000000002","name":"Kajol","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BNzIyNDI1MTYwMV5BMl5BanBnXkFtZTcwNzg5MzcxMw@@._V1_SY317_CR131,0,214,317_.jpg"},
+			{"_id":"53a6e01d8b587b0000000001","name":"Madhuri Dixit","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMjI1MTMxMDMxMV5BMl5BanBnXkFtZTcwMTUzNzY3Nw@@._V1_SY317_CR18,0,214,317_.jpg"},
+			{"_id":"53b6e01d8b587b0000000001","name":"Juhi Chawla","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BNzI1MzUxODczNV5BMl5BanBnXkFtZTcwNDUyNTA0MQ@@._V1_SY317_CR12,0,214,317_.jpg"},
+			{"_id":"5486e01d8b587b0000000001","name":"Arjun Rampal","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTg0NTAzOTU4Ml5BMl5BanBnXkFtZTcwMzg1MjUyOA@@._V1_SX214_CR0,0,214,317_.jpg"},
+			{"_id":"5586e01d8b587b0000000001","name":"Anupam Kher","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTY0MDkxMzkwN15BMl5BanBnXkFtZTcwODUxNTA5Nw@@._V1_SX214_CR0,0,214,317_.jpg"},
+			{"_id":"5686e01d8b587b0000000001","name":"Sanjay Dutt","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BNzU2NTgwNzY1OF5BMl5BanBnXkFtZTcwMjQxNzcxOA@@._V1_SY317_CR131,0,214,317_.jpg"},
+			{"_id":"5786e01d8b587b0000000001","name":"Hrithik Roshan","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTU1MjQzNDExN15BMl5BanBnXkFtZTcwNzIxMTg0Mw@@._V1_SY317_CR173,0,214,317_.jpg"},
+			{"_id":"5886e01d8b587b0000000001","name":"Akshay Kumar","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTU4NzM4Nzk1OF5BMl5BanBnXkFtZTcwMTAwMTA1NA@@._V1_SY317_CR104,0,214,317_.jpg"},
+			{"_id":"5986e01d8b587b0000000001","name":"Salman Khan","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTIyODQyOTA1M15BMl5BanBnXkFtZTcwMDQxNDIzMQ@@._V1_SY317_CR56,0,214,317_.jpg"},
+			{"_id":"5a86e01d8b587b0000000001","name":"Ranveer Singh","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTExOTcwMDU4NTReQTJeQWpwZ15BbWU3MDA0MjE1MTc@._V1_SY317_CR1,0,214,317_.jpg"},
+			{"_id":"5b86e01d8b587b0000000001","name":"Emraan Hashmi","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMjExNzExNDY3OV5BMl5BanBnXkFtZTcwNzAyMTg2OA@@._V1_SY317_CR0,0,214,317_.jpg"},
+			{"_id":"5c86e01d8b587b0000000001","name":"Amitabh Bachchan","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BNTk1OTUxMzIzMV5BMl5BanBnXkFtZTcwMzMxMjI0Nw@@._V1_SY317_CR8,0,214,317_.jpg"},
+			{"_id":"5d86e01d8b587b0000000001","name":"Abhishek Bachchan","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTk1NjU1NjkxN15BMl5BanBnXkFtZTcwNDI4MDMzMg@@._V1_SY317_CR10,0,214,317_.jpg"},
+			{"_id":"5e86e01d8b587b0000000001","name":"John Abraham","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMTY1MDUyMjI1N15BMl5BanBnXkFtZTYwMjg4MjA0._V1_SX214_CR0,0,214,317_.jpg"},
+			{"_id":"5f86e01d8b587b0000000001","name":"Deepika Padukone","category":["bollywood"],"img":"http://ia.media-imdb.com/images/M/MV5BMjAxMTAwMTY2MV5BMl5BanBnXkFtZTcwOTM5NTQyMg@@._V1_SX214_CR0,0,214,317_.jpg"}
+		]
+	},
+	"ended":false,
+	"turn":"52728fbf63a64c904c657ed5",
+	"state":"ask-question",
+	"modified":"2014-01-15T00:42:42.998Z"
+};
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.route('/');
+});
+
+App.AuthenticatedRoute = Ember.Route.extend({
+	beforeModel: function(transition) {
+		var loginController = this.controllerFor('login');
+		if (! loginController.get('user')) {
+			return loginController.login();
+		}
+	},
+	getJSONWithToken: function(url) {
+    	var user = this.controllerFor('login').get('user');
+    	var token = user.token;
+    	return $.getJSON(url, { token: token });
+  	},
+});
+
+App.ApplicationView = Ember.View.extend({
+	classNames: ["l-fill-parent"]
+});
+
+App.LoginController = Ember.Controller.extend({
+	login: function() {
+		var self = this;
+		var token = '52728fbf63a64c904c657ed5';
+		var data = {token:token};
+		return Ember.$.post('/api/login', data).then(function(response) {
+			self.set('user', {
+				id: response.id,
+				name: response.name,
+				token: response.token
+			});
+		}, function(err) {
+			console.log('login fail:');
+			console.log(JSON.stringify(err));
+		});
+	}
+});
+
+/** Game List **/
+App.IndexView = Ember.View.extend({
+	classNames: ["l-fill-parent", "l-container", 't-background']
+});
+
+App.IndexRoute = App.AuthenticatedRoute.extend({
+	model: function() {
+		return this.getJSONWithToken('/api/games');
+	}	
+});
+
+App.GameItemController = Ember.Controller.extend({
+	actions: {
+		select: function() {
+			console.log(this.get('model._id'));
+		}
+	},
+
+	date: function(key, value) {
+		var game = this.get('model');
+		return moment(game.modified).fromNow();
+	}.property('model.modified'),
+
+	turn: function(key, value) {
+		var game = this.get('model');
+		var turn = game.turn === game.me._id;
+		return turn ? "It's your turn" : "It's their turn";
+	}.property('model.turn'),
+
+	username: function(key, value) {
+		var game = this.get('model');
+		return game.opponent.username;
+	}.property('model.opponent.username')
+});
+
+/** Select Character **/
